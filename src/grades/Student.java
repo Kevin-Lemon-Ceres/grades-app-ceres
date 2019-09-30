e9a1138095a4b35d1da2f8bd146551bc4aab8599
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-30 11:43:43 -0500

    finished step one with get average method

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,5 +1,32 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
 
+    private String name;
+    private ArrayList<Integer> grades;
+
+    // constructor
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    // returns the student's name
+    public String getName(){
+        return this.name;
+    }
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double sum = 0;
+        for(Integer grade : this.grades){
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
 }
